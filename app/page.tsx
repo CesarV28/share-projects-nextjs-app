35a8cd2ca2defd366ff613eee72975dadb883778
Cesar Omar Vargas Gutierrez <CesarVargas27@outlook.com>
2023-06-24 10:21:14 -0600

    Navbar and Footer [created]

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='flex-start flex-col paddings mb-16'>
+        <h1>Categories</h1>
+        <h1>Post</h1>
+        <h1>Load more</h1>
+    </section>
   )
 }
 
